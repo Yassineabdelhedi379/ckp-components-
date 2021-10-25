8551e7c4f3116822b80bc25895e32a751da599c1
Yassineabdelhedi379 <yassine.abdelhedi4@gmail.com>
2021-10-25 17:03:27 +0200

    first commit

---
+++ src/Adress.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Adress = () => {
+    return (
+        <div>
+           <h2>Sfax,manzel chaker Klm 4,5</h2> 
+        </div>
+    )
+}
+
+export default Adress
---
+++ src/ProfilePhoto.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+export const ProfilePhoto = () => {
+    return (
+        <div>
+            <img src="profilePhoto" alt="photo.jpg" />
+        </div>
+    )
+}
+export default  ProfilePhoto;
